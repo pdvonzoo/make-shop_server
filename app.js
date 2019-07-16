0e357bb1f8e7f085d17a85dd1c36bc2ecadc7f9a
pdvonzoo <a01088570194@gmail.com>
2019-07-17 02:08:26 +0900

    Feat: connect DB

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
 
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("DB Connected"));
+
+// routes
 app.get("/", (req, res) => {
   res.send("hello world");
 });
