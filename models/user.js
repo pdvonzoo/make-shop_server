03c970afe9547082541798792a284f302d046d03
pdvonzoo <a01088570194@gmail.com>
2019-07-17 02:32:02 +0900

    Feat: hash password

--- models/user.js
+++ models/user.js
@@ -36,3 +36,31 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// virtual field
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    this._password = passowrd;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._passowrd;
+  });
+
+userSchema.methods = {
+  encryptPassword: function(password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
