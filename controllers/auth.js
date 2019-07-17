d9a12915dbe865cb29e193b845ccc02e313ab794
pdvonzoo <a01088570194@gmail.com>
2019-07-17 18:30:54 +0900

    Feat: product update

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,7 +49,7 @@ exports.requireSignin = expressJwt({
 });
 
 exports.isAuth = (req, res, next) => {
-  let user = req.profile && req.auth && req.profile._id === req.aut._id;
+  let user = req.profile._id && req.auth._id && req.profile._id == req.auth._id;
   if (!user) {
     return res.status(403).json({ error: "Access denied" });
   }
