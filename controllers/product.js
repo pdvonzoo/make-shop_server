7c498361a4faf4236a92cdf98ee92a8bb536be5b
pdvonzoo <a01088570194@gmail.com>
2019-07-17 14:59:03 +0900

    Feat: product image size, fields required logic

--- controllers/product.js
+++ controllers/product.js
@@ -15,7 +15,29 @@ exports.create = (req, res) => {
     }
     let product = new Product(fields);
 
+    // check foe all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are required"
+      });
+    }
+
     if (files.photo) {
+      // console.log("FILES PHOTO:", files.photo);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less than 1mb in size"
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
