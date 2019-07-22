389779252e7ff602b366da7b0587c4e680d81ae1
pdvonzoo <a01088570194@gmail.com>
2019-07-22 18:23:27 +0900

    Feat: Order routes and controller setup

--- routes/braintree.js
+++ routes/braintree.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+const { requireSignin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
-const { generateToken } = require("../controllers/braintree");
+const { generateToken, processPayment } = require("../controllers/braintree");
 
 router.get("/braintree/getToken/:userId", requireSignin, isAuth, generateToken);
-router.get("/braintree/payment/:userId", requireSignin, isAuth, processPayment);
+router.post(
+  "/braintree/payment/:userId",
+  requireSignin,
+  isAuth,
+  processPayment
+);
 
 router.param("userId", userById);
 
